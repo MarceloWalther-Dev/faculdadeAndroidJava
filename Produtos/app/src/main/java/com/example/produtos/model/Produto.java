5a7b5ad80a8891b7613678e29db17f51f3942dab
MarceloWalther-Dev <marcelo.walther.10@gmail.com>
2020-10-16 12:21:20 -0300

    Botão deletar

---
+++ Produtos/app/src/main/java/com/example/produtos/model/Produto.java
@@ -0,0 +1,51 @@
+package com.example.produtos.model;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+
+    private int id;
+    private String nome;
+    private float valor;
+
+
+    public Produto(int id, String nome, float valor) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return nome ;
+    }
+}
